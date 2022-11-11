fa5e04b1a45cf86074f34a2188ef5aa497218cdc
chaejin-jen <jen71324@gmail.com>
2022-11-11 18:05:08 +0900

    Refactoring namkim : separate new func: is_in_shadow

--- src/ray/phong_lighting.c
+++ src/ray/phong_lighting.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phong_lighting.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chaejkim <chaejkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 17:17:33 by chaejkim          #+#    #+#             */
+/*   Updated: 2022/11/11 18:03:03 by chaejkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ray.h"
 #include <math.h>
 
@@ -15,7 +27,8 @@ t_color3	phong_lighting(const t_scene *scene)
 	{
 		if (lights->type == LIGHT)
 		{
-			light_color = vadd_(light_color, point_light_get(scene, lights->element));
+			light_color = vadd_(light_color,
+					point_light_get(scene, lights->element));
 		}
 		lights = lights->next;
 	}
@@ -39,16 +52,26 @@ t_bool	in_shadow(t_object *obj, t_ray light_ray, float light_len)
 	return (FALSE);
 }
 
+t_bool	is_in_shadow(const t_scene *scene, const t_light *light, t_vec3 light_dir)//
+{
+	float	light_len;
+	t_vec3	v_tmp;
+	t_ray	light_ray;
+	
+	light_dir = vsub_(light->orig, scene->rec.p);
+	light_len = vnorm(light_dir);
+	v_tmp = vadd_(scene->rec.p, vmul(scene->rec.normal, EPSILON));
+	light_ray = ray_set(&v_tmp, &light_dir);
+	return (in_shadow(scene->object, light_ray, light_len));
+}
 
 static t_color3	point_light_get(const t_scene *scene, const t_light *light)
 {
-	t_color3	diffuse;
 	t_vec3		light_dir;
 	float		kd; // diffuse의 강도
 
-	float		light_len;
-	t_ray		light_ray;
 	float		brightness;
+	t_color3	diffuse;
 
 	t_color3	specular;
 	t_vec3		view_dir;
@@ -57,14 +80,9 @@ static t_color3	point_light_get(const t_scene *scene, const t_light *light)
 	float		ksn;
 	float		ks;
 
-	t_vec3		v_tmp;
-
 	light_dir = vsub_(light->orig, scene->rec.p);
-	light_len = vnorm(light_dir);
-	v_tmp = vadd_(scene->rec.p, vmul(scene->rec.normal, EPSILON));
-	light_ray = ray_set(&v_tmp, &light_dir);
-	if (in_shadow(scene->object, light_ray, light_len))
-		return (color3(0, 0, 0));
+	if (is_in_shadow(scene, light, light_dir))
+		return (vadd_(color3(0, 0, 0), scene->ambient));
 	light_dir = vunit(light_dir); //교점에서 출발하여 광원을 향하는 벡터(정규화 됨)
 	// cosΘ는 Θ 값이 90도 일 때 0이고 Θ가 둔각이 되면 음수가 되므로 0.0보다 작은 경우는 0.0으로 대체한다.
 	kd = fmax(vdot(scene->rec.normal, light_dir), 0.0);// (교점에서 출발하여 광원을 향하는 벡터)와 (교점에서의 법선벡터)의 내적값.
@@ -80,3 +98,44 @@ static t_color3	point_light_get(const t_scene *scene, const t_light *light)
 	brightness = light->bright_ratio * LUMEN;
 	return (vmul(vadd_(vadd_(diffuse, specular), scene->ambient), brightness));
 }
+
+// static t_color3	point_light_get(const t_scene *scene, const t_light *light)
+// {
+// 	t_color3	diffuse;
+// 	t_vec3		light_dir;
+// 	float		kd; // diffuse의 강도
+
+// 	float		light_len;
+// 	t_ray		light_ray;
+// 	float		brightness;
+
+// 	t_color3	specular;
+// 	t_vec3		view_dir;
+// 	t_vec3		reflect_dir;
+// 	float		spec;
+// 	float		ksn;
+// 	float		ks;
+
+// 	t_vec3		v_tmp;
+
+// 	light_dir = vsub_(light->orig, scene->rec.p);
+// 	light_len = vnorm(light_dir);
+// 	v_tmp = vadd_(scene->rec.p, vmul(scene->rec.normal, EPSILON));
+// 	light_ray = ray_set(&v_tmp, &light_dir);
+// 	if (in_shadow(scene->object, light_ray, light_len))
+// 		return (vadd_(color3(0, 0, 0), scene->ambient));
+// 	light_dir = vunit(light_dir); //교점에서 출발하여 광원을 향하는 벡터(정규화 됨)
+// 	// cosΘ는 Θ 값이 90도 일 때 0이고 Θ가 둔각이 되면 음수가 되므로 0.0보다 작은 경우는 0.0으로 대체한다.
+// 	kd = fmax(vdot(scene->rec.normal, light_dir), 0.0);// (교점에서 출발하여 광원을 향하는 벡터)와 (교점에서의 법선벡터)의 내적값.
+// 	diffuse = vmul(light->light_color, kd);
+
+// 	view_dir = vunit(vmul(scene->ray.dir, -1));
+// 	reflect_dir = reflect(vmul(light_dir, -1), scene->rec.normal);
+// 	ksn = 64; // shininess value
+// 	ks = 0.7; // specular strength
+// 	spec = pow(fmax(vdot(view_dir, reflect_dir), 0.0), ksn);
+// 	specular = vmul(light->light_color, ks * spec);
+
+// 	brightness = light->bright_ratio * LUMEN;
+// 	return (vmul(vadd_(vadd_(diffuse, specular), scene->ambient), brightness));
+// }
